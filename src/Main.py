1b1b4863c9ffc7a5ccc26bfd6eef370aaa28ff7e
Alejosebasp <alsalejopa@unal.edu.co>
2019-07-21 12:28:16 -0500

    Implementando un listener de prueba

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,15 @@
 from src import dis
 import os
+from antlr4 import *
+
+from gen.asm8080Lexer import asm8080Lexer as Lexer
+from gen.asm8080Parser import asm8080Parser as Parser
+from gen.asm8080Listener import asm8080Listener as Listener
+from gen.asm8080Visitor import asm8080Visitor as Visitor
+
+class listenerRegister(Listener):
+    def enterRegister_(self, ctx:Parser.Register_Context):
+        print(ctx.getText())
 
 if __name__ == '__main__':
 
@@ -8,3 +18,14 @@ if __name__ == '__main__':
     print(dirpath)
     dis.desensamblar(dirpath, "invaders")
     print("listo")
+
+    inputStream = FileStream(r"C:\Users\Alejosebasp\Documents\UNAL\Lenguajes\ADHY\inputs\invaders_dis.txt")
+
+    lexer = Lexer(inputStream)
+    stream = CommonTokenStream(lexer)
+    parser = Parser(stream)
+    tree = parser.prog()
+
+    RegisterPrint = listenerRegister()
+    walker = ParseTreeWalker()
+    walker.walk(RegisterPrint, tree)
